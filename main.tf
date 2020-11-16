be24d25efcc3aabec3517c0bf2aab94c51810de7
= <=>
2020-11-17 00:01:29 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -143,12 +143,6 @@ resource "aws_instance" "web-server-instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              # sudo apt-get update
-              # sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-              # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-              # sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
-              # sudo apt-get update
-              # sudo apt-get install docker-ce docker-ce-cli containerd.io
               sudo apt update -y
               sudo apt install apache2 -y
               sudo systemctl start apache2
