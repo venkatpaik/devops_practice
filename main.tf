d8a591008faeb8dd47a7f4013a7de756c9ac8691
= <=>
2020-11-17 20:27:28 +0530

    Modified main.tf

--- main.tf
+++ main.tf
@@ -128,9 +128,7 @@ output "server_public_ip" {
   value = aws_eip.one.public_ip
 }
 
-variable "credentials_file" {
-
-}
+#variable "credentials_file" {}
 
 # # 9. Create ubuntu instance and install/enable docker
 
